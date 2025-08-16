6f27827ffa1b16114e20a427465d235255ae3a1d
necto4529-droid <necto4529@gmail.com>
2025-08-16 23:12:34 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: 'dist'
+  base: '/NovaGram/', // название твоего репозитория
+  server: {
+    port: 3000,
   }
 })
